dceee1d1668cc2c27a7709d8b7241c418587e11e
Tony3201 <Tony3201@users.noreply.github.com>
2020-04-17 01:23:22 -0400

    Update Tmux for Home Workspace case[1].sh

--- Tmux for Home Workspace case[1].sh
+++ Tmux for Home Workspace case[1].sh
@@ -1,8 +1,8 @@
 #########################################################################
 # Name: TmuxForHomeWorkspaceCase[1]
-# Version: 1.0
+# Version: 1.1
 # Author: Tony3201 
-# Created Time: Apr 08 2020		
+# Created Time: Apr 17 2020		
 #########################################################################
 #Need to install firefox,fortune,htop,iftop and tree command.This script just need normal permission for iftop to show 
 #the ipaddr,or you can use ifconfig command instead it.
@@ -19,9 +19,9 @@ tmux split-window -h
 tmux split-window -h 'Net'
 
 tmux send-keys -t 1.1 tree  Enter
-#tmux send-keys -t 1.2 htop Enter
+tmux send-keys -t 1.2 htop Enter
 tmux send-keys -t 1.3 fortune  Enter
-tmux send-keys -t 1.4 iftop Enter
+tmux send-keys -t 1.4 nmcli Enter
 tmux send-keys -t 1.5 firefox Enter
 
 
